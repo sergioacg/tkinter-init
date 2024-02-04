119ed1942b98fc90b50e297b028333208f5c8f69
Sergio Giraldo <sergio_castano91103@elpoli.edu.co>
2024-02-04 00:14:09 -0300

    Add window resizing options

--- tk.py
+++ tk.py
@@ -4,5 +4,9 @@ from tkinter import *   # Importa el módulo tkinter
 root = Tk()             # Crea una ventana
 root.title("Hola Mundo")       # Título de la ventana
 root.iconbitmap('favicon.ico') # Icono de la ventana
+# (1, 1) deja dimensionar ancho y largo
+# (0, 0) no deja dimensionar ancho y largo
+# (1, 0) deja dimensionar ancho y no largo
+# (0, 1) no deja dimensionar ancho y deja largo
 root.resizable(1, 1)    # Permite redimensionar la ventana
 root.mainloop()         # Muestra la ventana
